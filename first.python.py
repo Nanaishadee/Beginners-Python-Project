dd28eadf95a30e228b68a45a0008165f0f35fd54
OLUWAFEMISOLA AISHA BRAIMOH-IGBO <nanaishadee@gmail.com>
2021-02-16 13:39:46 -0600

    Add files via upload

---
+++ first.python.py
@@ -0,0 +1,30 @@
+import random
+
+def guess(x):
+    random_number= random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess > random_number:
+            print("Ooops, guess again. Too low")
+        elif guess < random_number:
+            print("Got ya, better luck next time. Too high")
+
+    print(f"Someone got luck today!! You have  the number {random_number} right ")
+def program_guess(x):
+    low = 1
+    high = x
+    feedback = " "
+    while feedback != "c": 
+        if low != high:
+             guess = random.randint(low, high) 
+        else:
+            guess = low 
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)? .lower() ")
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low = guess + 1
+        
+    print(f"Bravo! The program guessed your number, {guess} correctly!" )
+program_guess(1000)
---
+++ madlib.py
@@ -0,0 +1,9 @@
+name= input("Name: ")
+adj= input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2= input("Verb2: ") 
+famous_person= input("Famous Person: ")
+
+madlib =  f"Hi, my name is {name}. I have found computer programming to be {adj}! It makes me so excited all the time knowing I can learn new things \
+Aside coding,I love to watch {verb1} and hang out with {verb2}. I hope to become like {famous_person} at coding."
+print(madlib)
